53ecd61341716c7b4513f0bfbacfa350d731aaa6
Carmen Díez Menéndez <dmcarmen1999@gmail.com>
2022-05-09 22:12:21 +0200

    .

--- mi_euler.m
+++ mi_euler.m
@@ -7,8 +7,8 @@ function  [t, y] = mi_euler(f,t0,T,y0,h)
     %   y0: Y(t0)=y0, vector con el valor inicial. 
     %   h: tamanio de las particionnes de en el intervalo [t0,T].
     % return:
-    %   t: los ts donde se ha evaluado la función (es decir, [t0,T] 
-    %        dividido en N pasos).
+    %   t: los ts donde se ha evaluado la función 
+    %      (es decir, [t0,T] dividido en N pasos).
     %   y: la matriz solucion.
 
     N=(T-t0)/h;     % Numero de divisiones
@@ -17,7 +17,7 @@ function  [t, y] = mi_euler(f,t0,T,y0,h)
     % Inicializamos la matriz a 0, excepto el valor incial
     y=zeros(length(y0),N+1); 
     y(:,1)=y0;
-    
+
     for n=1:N
         % Euler explicito: y_{n+1}=y_n+h*f(t_n,y_n)
         y(:,n+1)=y(:,n)+h*f(t(n),y(:,n));
