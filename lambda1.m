1b20878de5224645c5b1402fe0940e72f0432c27
Carmen Díez Menéndez <51020444+dmcarmen@users.noreply.github.com>
2022-05-27 15:42:54 +0200

    Update lambda1.m

--- lambda1.m
+++ lambda1.m
@@ -1,7 +1,6 @@
 function [res] = lambda1(q)
     %LAMBDA1 Funcion lambda para el primer par de variables. 
-    % Representa los ratios de transicion debido a mutaciones de las 
-    % celulas de X1 (AD) a las de X2 (AI).
+    % Representa los ratios de transicion debido a mutaciones de las celulas de X1 (AD) a las de X2 (AI).
     % Params:
     %   q: valor de Q(t).
     % return:
